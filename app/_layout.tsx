fec87fa5b7e00b9ab89921f1b5780a7beb28dede
OGagnon102 <ogagnon102@gmail.com>
2026-04-29 17:09:07 -0400

    build debug fonctionne

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function Layout() {
   return (
     <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: true }} />
+      <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
   );
 }
